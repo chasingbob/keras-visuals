ce97eafdd586f032181d7f05dd5d1eb5dd482b74
Chasingbob <dries.cronje@outlook.com>
2017-01-09 05:52:15 +0200

    Initial commit

---
+++ visual_callbacks.py
@@ -0,0 +1,75 @@
+from keras.callbacks import Callback
+import matplotlib.pyplot as plt    
+import matplotlib.patches as mpatches  
+
+
+class AccLossPlotter(Callback):
+    """Plot training Accuracy and Loss values on a Matplotlib graph. 
+
+    The graph is updated by the 'on_epoch_end' event of the Keras Callback class
+
+    # Arguments
+        graphs: list with some or all of ('acc', 'loss')
+
+    """
+
+    def __init__(self, graphs=['acc', 'loss'], save_graph=False):
+        self.graphs = graphs
+        self.num_subplots = len(graphs)
+        self.save_graph = save_graph
+
+
+    def on_train_begin(self, logs={}):
+        self.acc = []
+        self.val_acc = []
+        self.loss = []
+        self.val_loss = []
+        self.epoch_count = 0
+        plt.ion()
+        plt.show()
+
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.epoch_count += 1
+        self.val_acc.append(logs.get('val_acc'))
+        self.acc.append(logs.get('acc'))
+        self.loss.append(logs.get('loss'))
+        self.val_loss.append(logs.get('val_loss'))
+        epochs = [x for x in range(self.epoch_count)]
+
+        count_subplots = 0
+        
+        if 'acc' in self.graphs:
+            count_subplots += 1
+            plt.subplot(self.num_subplots, 1, count_subplots)
+            plt.title('Accuracy')
+            #plt.axis([0,100,0,1])
+            plt.plot(epochs, self.val_acc, color='r')
+            plt.plot(epochs, self.acc, color='b')
+            plt.ylabel('accuracy')
+
+            red_patch = mpatches.Patch(color='red', label='Test')
+            blue_patch = mpatches.Patch(color='blue', label='Train')
+
+            plt.legend(handles=[red_patch, blue_patch], loc=4)
+
+        if 'loss' in self.graphs:
+            count_subplots += 1
+            plt.subplot(self.num_subplots, 1, count_subplots)
+            plt.title('Loss')
+            #plt.axis([0,100,0,5])
+            plt.plot(epochs, self.val_loss, color='r')
+            plt.plot(epochs, self.loss, color='b')
+            plt.ylabel('loss')
+
+            red_patch = mpatches.Patch(color='red', label='Test')
+            blue_patch = mpatches.Patch(color='blue', label='Train')
+
+            plt.legend(handles=[red_patch, blue_patch], loc=4)
+        
+        plt.draw()
+        plt.pause(0.001)
+
+    def on_train_end(self, logs={}):
+        if self.save_graph:
+            plt.savefig('training_acc_loss.png')
